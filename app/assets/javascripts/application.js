c8bb0a2a7b03542f2357b5e171d29c8dcaf89972
dima <stoyanovd@mail.ru>
2015-04-01 02:47:03 +0300

    Names of arts. A lot of small fixes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,26 +19,31 @@ function initPage() {
     // Page ready code...
 
 
-        $(".land_word_illustration").hover(function () {
-            $(this).closest(".land_illustration").toggleClass("opacity_illustration");
-        });
-        $(".land_word_lettering").hover(function () {
-            $(this).closest(".land_lettering").toggleClass("opacity_lettering");
-        });
-        $(".land_word_logo").hover(function () {
-            $(this).closest(".land_logo_inner_container").toggleClass("opacity_logo");
-        });
+    $(".land_word_illustration").hover(function () {
+        $(this).closest(".land_illustration").toggleClass("opacity_illustration");
+    });
+    $(".land_word_lettering").hover(function () {
+        $(this).closest(".land_lettering").toggleClass("opacity_lettering");
+    });
+    $(".land_word_logo").hover(function () {
+        $(this).closest(".land_logo_inner_container").toggleClass("opacity_logo");
+    });
 
-        $(".menu_item").hover(function () {
-            $(this).children(".menu_word").toggleClass("hidden");
-        });
-        $(".container_last").hover(function () {
-            $(this).find(".mail_to_animate").addClass("mail_animation");
-            $(this).find(".vk_to_animate").addClass("vk_animation");
-        }, function () {
-            $(this).find(".mail_to_animate").removeClass("mail_animation");
-            $(this).find(".vk_to_animate").removeClass("vk_animation");
-        });
+    $(".menu_item").hover(function () {
+        $(this).children(".menu_word").toggleClass("hidden");
+    });
+    $(".container_last").hover(function () {
+        $(this).find(".mail_to_animate").addClass("mail_animation");
+        $(this).find(".vk_to_animate").addClass("vk_animation");
+    }, function () {
+        $(this).find(".mail_to_animate").removeClass("mail_animation");
+        $(this).find(".vk_to_animate").removeClass("vk_animation");
+    });
+
+    $(".letterings_inner").hover(function () {
+        $(this).children(".letterings_inner_image").toggleClass("half_opacity");
+        $(this).children(".letterings_inner_text").toggleClass("hidden");
+    });
 
 }
 
